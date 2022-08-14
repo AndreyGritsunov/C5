17510c6e020cea789f0887d4f9111e1057533fd1
unknown <gritsunov85@gmail.com>
2022-08-14 09:50:04 +0600

    End

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите 3-х значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100 || number > 999){
+
+    Console.WriteLine("Вы ввели не правильное число!");    
+} 
+else{
+     while (number >= 100){
+
+            number /= 10;
+    }
+    Console.WriteLine(number % 10);
+
+}
+       
+
